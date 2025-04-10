ce26a4ca8da7e2e1dbec046c5c991a3e40aba206
n0sat1y <sashamorozov533@gmail.com>
2025-04-10 10:35:39 +0300

    config + db settings

---
+++ src/core/db.py
@@ -0,0 +1,22 @@
+from sqlalchemy.ext.asyncio import create_async_engine, async_sessionmaker
+
+from src.core.config import settings
+
+class DataBase:
+	def __init__(self):
+		self.engine = create_async_engine(
+			url=settings.DB_URL
+		)
+		self.sessionlocal = async_sessionmaker(
+			bind=self.engine,
+			expire_on_commit=False
+		)
+
+	async def session(self):
+		async with self.sessionlocal() as session:
+			yield session
+
+	async def close(self):
+		await self.engine.dispose()
+
+db = DataBase()

6923d03775aa5eaf106c8674e11c1761075d3e79
n0sat1y <sashamorozov533@gmail.com>
2025-04-10 10:59:03 +0300

    alembic

--- src/core/db.py
+++ src/core/db.py
@@ -1,5 +1,22 @@
+from sqlalchemy import MetaData
 from sqlalchemy.ext.asyncio import create_async_engine, async_sessionmaker
+from sqlalchemy.orm import DeclarativeBase
+
 from src.core.config import settings
+
+convention = {
+    "all_column_names": lambda constraint, table: "_".join(
+        [column.name for column in constraint.columns.values()]
+    ),
+    "ix": "ix__%(table_name)s__%(all_column_names)s",
+    "uq": "uq__%(table_name)s__%(all_column_names)s",
+    "ck": "ck__%(table_name)s__%(constraint_name)s",
+    "fk": "fk__%(table_name)s__%(all_column_names)s__%(referred_table_name)s",
+    "pk": "pk__%(table_name)s",
+}
+
+class Base(DeclarativeBase):
+	metadata = MetaData(naming_convention=convention)
 
 class DataBase:
 	def __init__(self):
--- src/models/__init__.py
+++ src/models/__init__.py
@@ -1,20 +1,10 @@
 from sqlalchemy import MetaData	
 from sqlalchemy.orm import DeclarativeBase
-
-convention = {
-    "all_column_names": lambda constraint, table: "_".join(
-        [column.name for column in constraint.columns.values()]
-    ),
-    "ix": "ix__%(table_name)s__%(all_column_names)s",
-    "uq": "uq__%(table_name)s__%(all_column_names)s",
-    "ck": "ck__%(table_name)s__%(constraint_name)s",
-    "fk": "fk__%(table_name)s__%(all_column_names)s__%(referred_table_name)s",
-    "pk": "pk__%(table_name)s",
-}
-
-class Base(DeclarativeBase):
-	metadata = MetaData(naming_convention=convention)
+	
+from src.models.tables import TableModel
+from src.models.reservations import ReservationModel
 
 __all__ = [
-	"Base",
+	"TableModel",
+    "ReservationModel",
 ]
